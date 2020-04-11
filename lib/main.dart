b71404e75642b6001a64ebec3933e0b62e7d2cb2
Achinths Isuru <achinthaisuru444@gmail.com>
2020-04-12 01:27:19 +0530

    small changes

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'CLOCK',
       theme: ThemeData(
         fontFamily: 'BowlbyOneSC',
+        primaryColor: Colors.black,
       ),
       home: Clock(),
     );
